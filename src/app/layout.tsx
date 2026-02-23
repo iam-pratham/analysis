13a0facf1088deda02a945f460020e3be7936b74
prathamm-99 <101522954+iam-pratham@users.noreply.github.com>
2026-02-23 18:49:47 +0530

    Set default theme to light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           <TooltipProvider>
             <LayoutWrapper>
               {children}
